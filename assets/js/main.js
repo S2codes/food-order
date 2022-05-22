94350bc19c8b4321da9b2d41a6f4dcdb9f77792c
S2codes <25subhankar@gmail.com>
2022-05-22 21:44:19 +0530

    add responsive style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
-    if (scroll > 20) {
-        $("#mainNav").removeClass("navbar-light");
+    if (scroll > 90) {
         $("#mainNav").addClass("stickyNav");
+        $("#mainNav").removeClass("navbar-light");
         $("#mainNav").addClass("navbar-dark");
         $("#callus").addClass("text-light");
     } else {
         $("#mainNav").removeClass("stickyNav");
+        $("#mainNav").removeClass("navbar-dark");
         $("#mainNav").addClass("navbar-light");
         $("#callus").removeClass("text-light");
     }
