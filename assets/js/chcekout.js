9c6a7830882d37528a16e31a9ec189a9150a0bd1
S2codes <25subhankar@gmail.com>
2022-05-25 22:03:33 +0530

    add reservation and cupon funcanality

--- assets/js/chcekout.js
+++ assets/js/chcekout.js
@@ -27,4 +27,104 @@ $(document).ready(function () {
         $('#totalItemprice'+id).text('₹'+(parseInt(t_price)-parseInt(item_price)));
 
     })
+
+ // add cupon 
+ $('#addCuponBtn').click(function () {
+    // console.log('add cupon');
+    $('#cuponModal').modal('show');
+})
+$('#addAddress').click(function () {
+    // console.log('add cupon');
+    $('#addressModal').modal('show');
+})
+
+
+
+
+$('#cuponModaform').on('submit', function (e) {
+    e.preventDefault();
+    console.log('modal clikc');
+    // $('#applyCupon').click();
+    let cuponcode = $('#cuponInput').val();
+    showCupon(cuponcode, 50)
+    
+})
+$('#applyCupon').on('click', function (e) {
+    e.preventDefault();
+    console.log('modal clikc');
+    // $('#applyCupon').click();
+    let cuponcode = $('#cuponInput').val();
+    showCupon(cuponcode, 50)
+    
+})
+
+// $('#applyCupon').click(function (e) {
+//     e.preventDefault();
+//     console.log('apply cupon');
+//     let cuponcode = $('#cuponInput').val();
+//     if (cuponcode != '') {
+//         getCupon(cuponcode, function callback(data) {
+//             data = JSON.parse(data)
+//             console.log('parse ', data);
+//             console.log('stringy ', data.id);
+//             let all_totalprice = parseInt($('#totalPrice').val());
+//             console.log(all_totalprice);
+//             // cuponOff = data.
+//             const minAmount = parseInt(data.min_amount)
+//             let discount = parseInt(data.discount)
+//             console.log(typeof (minAmount));
+//             console.log(typeof (all_totalprice));
+//             if (data.status == "Available") {
+//                 $('#cuponAlert').text('');
+//                 if (minAmount <= all_totalprice) {
+//                     $('#cuponAlert').text('');
+//                     console.log('cupon applicable');
+//                     if (data.type == 'f') {
+//                         console.log(discount);
+//                         showCupon(cuponcode, discount);
+//                     }
+//                     else if (data.type == 'p') {
+//                         discount = (discount / 100);
+//                         console.log('pasentage of ', discount);
+//                         let dicountPrice = Math.round((all_totalprice * discount));
+//                         console.log('total dicount ', all_totalprice);
+//                         showCupon(cuponcode, dicountPrice);
+//                     }
+
+//                 } else {
+//                     $('#cuponAlert').text(`Not Applicable, Minimum Amount ₹${minAmount}`);
+//                 }
+//             } else {
+//                 $('#cuponAlert').text(`No Longer Available`);
+//             }
+
+
+//         })
+//     }
+
+// })
+
+function showCupon(cupon, discount) {
+    // console.log('cupon ', cupon);
+    // console.log('discount ', discount);
+
+
+    console.log(cupon);
+    $('#cuponModal').modal('hide');
+    $('#cuponConatiner').html(`  <li class="text-primary">
+    Coupon <span class="cupon">(${cupon})</span>
+</li>
+<li class="text-primary">-₹${discount}</li>`)
+
+    // let totalPrice = $('#totalPrice').val();
+    // console.log('grand price ', (totalPrice - discount));
+    // let grand_price = (totalPrice - discount);
+    // $('#grandPrice_input').val(grand_price);
+    // $('#grandPrice').text('₹' + grand_price);
+
+
+}
+
+
+
 })
